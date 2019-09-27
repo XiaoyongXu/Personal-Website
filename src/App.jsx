85b77c8f17bc3e810e4154190ae2d521e99aebab
XiaoyongXu <xxy1994212@gmail.com>
2019-09-26 23:51:32 -0700

    try some image

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,60 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import './App.css'
+import Vancouver2 from './styles/images/Vancouver2.png'
+import Vancouver1 from './styles/images/Vancouver1.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+    this.myRef = React.createRef()
+    this.handleScrollToElement = this.handleScrollToElement.bind(this)
+  }
+
+  componentDidMount() {
+  }
+
+  handleScrollToElement() {
+    window.scrollTo(0, this.myRef.current.offsetTop)
+  }
+
+  render() {
+    return (
+      <div className="App" style={{ scrollBehavior: 'smooth' }}>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home" onClick={this.handleScrollToElement}>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>
+          <img src={Vancouver2} alt="vancouver" style={{ display: 'flex', flex: 1, width: '100vw' }} />
+        </div>
+        <div>
+          <img ref={this.myRef} src={Vancouver1} alt="vancouver" style={{ display: 'flex', flex: 1, width: '100vw' }} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
